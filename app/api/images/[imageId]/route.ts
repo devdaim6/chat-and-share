07c81a85313189646fe2079d722edb7ad9f8b8f3
devdaim6 <daimdev6@gmail.com>
2024-11-15 20:21:04 +0530

    Error Fix : `NextRequest Never used`

--- app/api/images/[imageId]/route.ts
+++ app/api/images/[imageId]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { Image, connectDB } from "@/app/lib/models";
 
 export async function GET(
