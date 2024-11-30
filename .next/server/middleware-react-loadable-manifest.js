cad9018783bcf6ddca7ee4a813ba415091c64cf1
devdaim6 <daimdev6@gmail.com>
2024-11-30 16:43:51 +0530

    Build Upload

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST='{"app\\\\chat\\\\page.tsx -> @/components/ChatRoom":{"id":4765,"files":["static/chunks/776-11ebe45f8c876d55.js","static/chunks/765.e398efb76a2b2a6c.js"]}}';
