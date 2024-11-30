74f26a31faba8ac75435851b5549a233c185343a
devdaim6 <daimdev6@gmail.com>
2024-11-30 14:43:40 +0530

    build script update

--- build.sh
+++ build.sh
@@ -4,14 +4,6 @@
 echo "Pulling latest changes from the main branch..."
 git pull origin main || { echo "git pull failed"; exit 1; }
 
-# Install dependencies with force
-echo "Installing dependencies with force..."
-npm i --force || { echo "npm install failed"; exit 1; }
-
-# Build the application
-echo "Building the application..."
-npm run build || { echo "Build failed"; exit 1; }
-
 # Restart PM2 processes
 echo "Restarting PM2 processes..."
 pm2 delete all || echo "No PM2 processes to delete."
